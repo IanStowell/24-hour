0a19d2a8842f210b8e385b743dfda536060c2d9f
IanStowell <ianrstowell@gmail.com>
2020-10-21 17:52:46 -0400

    first commit

---
+++ SocialMedia.Data/Like.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Data
+{
+    public class Like
+    {
+        [ForeignKey(nameof(PostID))]
+        public int PostID;
+        public virtual Post LikedPost { get; set; }
+        public virtual User Liker { get; set; }
+    }
+}
---
+++ SocialMedia.Models/LikeCreate.cs
@@ -0,0 +1,21 @@
+﻿using SocialMedia.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class LikeCreate
+    {
+        [Required]
+        public string Name;
+        [Required]
+        public int PostID;
+        public virtual Post LikedPost { get; set; }
+        public virtual User Liker { get; set; }
+    }
+}
